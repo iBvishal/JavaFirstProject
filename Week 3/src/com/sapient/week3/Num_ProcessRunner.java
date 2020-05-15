5b439f33f199c04c5e9c2c50811600642b290b51
VISHAL <bhardwaj.vishalnit@outlook.com>
2020-05-15 22:17:05 +0530

    a change is num_process

--- Week 3/src/com/sapient/week3/Num_ProcessRunner.java
+++ Week 3/src/com/sapient/week3/Num_ProcessRunner.java
@@ -1,11 +1,15 @@
 package com.sapient.week3;
 
-public class Num_ProcessRunner implements Num_Process {
-	
-	@Override
-	public double cal(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+public class Num_ProcessRunner {
+	public static void main(String[] args) {
+		Num_Process ob[] = new Num_Process[4];
+		ob[0] = (a,b)-> (a+b);
+		ob[1] = (a,b)-> (a-b);
+		ob[2] = (a,b)-> (a*b);
+		ob[3] = (a,b)-> (a/b);
+		int n = Read.readInt();
+		int a = Read.readInt();
+		int b = Read.readInt();
+		System.out.println(ob[n-1].cal(a, b));
 	}
-
 }
